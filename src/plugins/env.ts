3fb23a8e61d3f0f1fee8e180f2baf9a7a96e5051
topsinoty <topsintoty@gmail.com>
2026-01-30 13:31:36 +0200

    setup env

---
+++ src/plugins/env.ts
@@ -0,0 +1,17 @@
+import fp from 'fastify-plugin'
+import env from '@fastify/env'
+
+export default fp(async (fastify) => {
+  await fastify.register(env, {
+    schema: {
+      type: 'object',
+      required: ['PORT'],
+      properties: {
+        PORT: { type: 'number', default: 8080 },
+        HOST: { type: 'string', default: 'localhost' },
+        NODE_ENV: { type: 'string', default: 'development' }
+      }
+    },
+    dotenv: true 
+  })
+})
